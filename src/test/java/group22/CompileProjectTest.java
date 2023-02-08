45ce3efaacaabc432699d25499a424fbad97b555
regusta <regusta@kth.se>
2023-02-08 15:33:10 +0100

    fix #11 removed errant slash in local path

--- src/test/java/group22/CompileProjectTest.java
+++ src/test/java/group22/CompileProjectTest.java
@@ -18,7 +18,7 @@ public class CompileProjectTest {
     @Test
     public void testPositiveCompileProject() throws IOException {
         String repositoryURL = "https://github.com/ppplbngth/CI-server.git";
-        String localPath = "/test-repository-compile";
+        String localPath = "test-repository-compile";
         String branch = "main";
         CloneRepository.cloneRepository(repositoryURL, localPath, branch);
         //File localRepository = new File(localPath);
